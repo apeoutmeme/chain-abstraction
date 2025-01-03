1a8ea9b5330839c553015dca0be0371201f572ef
Apeout <apeoutcrypto@gmail.com>
2025-01-02 20:04:19 -0800

    Add files via upload

---
+++ chains.js
@@ -0,0 +1,20 @@
+const chains = {
+    Ethereum: {
+      chainId: "0x1",
+      rpcUrl: "https://mainnet.infura.io/v3/YOUR_INFURA_KEY",
+      name: "Ethereum Mainnet",
+    },
+    BSC: {
+      chainId: "0x38",
+      rpcUrl: "https://bsc-dataseed.binance.org/",
+      name: "Binance Smart Chain",
+    },
+    Solana: {
+      chainId: "solana",
+      rpcUrl: "https://api.mainnet-beta.solana.com",
+      name: "Solana Mainnet",
+    },
+  };
+  
+  export default chains;
+  
